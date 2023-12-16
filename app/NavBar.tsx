b5d120c9a77b1cefbe7e11913cfe6c3c8255fabc
Julio Uribe <uribejulioc@gmail.com>
2023-12-16 08:54:19 -0800

    Add register page for NextAuth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,26 @@
 "use client";
 
 import { Skeleton } from "@/app/components";
-import { Button, Container, Flex } from "@radix-ui/themes";
+import { Box, Button, Container, Flex } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-
 const NavBar = () => {
+
+  return (
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between" align="center">
+          <div>Top Left</div>
+          <div>Center</div>
+          <UserStatus />
+        </Flex>
+      </Container>
+    </nav>
+  )
+}
+
+const UserStatus = () => {
   const router = useRouter()
   const { status, data: session } = useSession()
 
@@ -18,27 +32,21 @@ const NavBar = () => {
   }
 
   return (
-    <nav className="border-b mb-5 px-5 py-3">
-      <Container>
-        <Flex justify="between" align="center">
-          <div>Top Left</div>
-          <div>Center</div>
-          {status === "authenticated" && (
-            <Button onClick={handleLogout}>
-              Logout
-            </Button>
-          )}
-          {status === "unauthenticated" && (
-            <Button onClick={handleLogin}>
-              Login
-            </Button>
-          )}
-          {status === "loading" && (
-            <Skeleton width="2em" />
-          )}
-        </Flex>
-      </Container>
-    </nav>
+    <Box>
+      {status === "authenticated" && (
+        <Button onClick={handleLogout} className="hover:cursor-pointer">
+          Logout
+        </Button>
+      )}
+      {status === "unauthenticated" && (
+        <Button onClick={handleLogin} className="hover:cursor-pointer">
+          Login
+        </Button>
+      )}
+      {status === "loading" && (
+        <Skeleton width="2em" />
+      )}
+    </Box>
   )
 }
 
---
+++ app/api/register/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+import bcrypt from "bcrypt";
+
+const schema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export async function POST(request: NextRequest) {
+  // This endpoint is used by NextAuth to register a new user, including forms
+  const body = await request.json();
+
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const user = await prisma.user.findUnique({ where: { email: body.email } });
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(newUser);
+}
