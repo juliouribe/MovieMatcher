0e8a94ade53377a9593c5ba49d2efd4ccfdb36e7
Julio Uribe <uribejulioc@gmail.com>
2023-12-16 07:56:03 -0800

    Add conditional login controls to NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,18 +1,29 @@
-'use client';
+"use client";
 
-import { Container, Flex } from '@radix-ui/themes';
-import Link from 'next/link';
-import React from 'react';
+import { Container, Flex } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 
 const NavBar = () => {
+  const { status, data: session } = useSession()
+
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
         <Flex justify="between" align="center">
           <div>Top Left</div>
           <div>Center</div>
-          <Link href="/api/auth/signin">Login</Link>
+          { status === "authenticated" && (
+            <Link href="/api/auth/signout">Logout</Link>
+          )}
+          { status === "unauthenticated" && (
+            <Link href="/api/auth/signin">Login</Link>
+          )}
+          { status === "loading" && (
+            <div>Loading...</div>
+          )}
         </Flex>
       </Container>
     </nav>
